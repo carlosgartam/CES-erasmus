090f6a4f84bb152b1a4899bc433510c1a5893fa7
Javier <javierru8@outlook.es>
2021-12-19 20:45:13 +0100

    Merge branch 'main' of https://github.com/carlosgartam/CES-erasmus into main

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-//import {db,useFirebaseApp} from '../firebase'
+import {app} from '../firebase'
 import 'firebase/auth'
 
 
@@ -8,27 +8,42 @@ export default function User () {
         email:'',
         password:''
     })
-    //const firebase = useFirebaseApp()
-
-    const register = async () => {
-           
-    }
+    let localUser=''
     const onInput=(e)=>{
         setUser({...User,[e.target.name]: e.target.value})
+        console.log(User)
     }
-    const onSubmit=(e)=>{
-        
+
+    const SingUp= async ()=>{
+        await app.auth().createUserWithEmailAndPassword(User.email,User.password)
+        console.log('singup')
+        localUser= User.email;
     }
+    const LogIn=(e)=>{
+        app.auth().createUserWithEmailAndPassword(User.email,User.password)
+    }
+
+
     console.log(User)
     
     return( 
     <div>
         <div>
+            User:{localUser}
+        </div>
+        <div>
             <label htmlFor="email">e-mail</label>
             <input type="email" id="email" name='email' onChange={onInput}></input>
             <label htmlFor="password">password</label>
             <input type="password" id="password" name='password' onChange={onInput}></input>
-            <button onChange={onSubmit}>Register</button>
+            <button onClick={SingUp}>Sing up</button>
+        </div>
+        <div>
+            <label htmlFor="email">e-mail</label>
+            <input type="email" id="email" name='email' onChange={onInput}></input>
+            <label htmlFor="password">password</label>
+            <input type="password" id="password" name='password' onChange={onInput}></input>
+            <button onClick={LogIn}>Login</button>
         </div>
     </div>
     );
