147cc747682b97b27396b206e9f808e0ba34ebea
Javier <javierru8@outlook.es>
2021-12-26 17:03:51 +0100

    Login

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 
 import './App.css';
 import './firebase';
+import 'firebase/auth';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
 import {Routes, BrowserRouter,Route, Link} from 'react-router-dom'
 import ListSubjects from './components/ListSubjects';
 import Subject from './components/Subject';
-import User from './components/User';
+import LogIn from './components/LogIn';
+import SingUp from './components/SingUp';
+import PrivateRoute from "./components/PrivateRoute";
+import {AuthProvider} from './components/Auth';
+import {app} from './firebase'
 function App() {
   return (
+    <AuthProvider>
     <div> 
-        
-      <BrowserRouter> 
-      <Nav/>
+      <BrowserRouter>
       <Routes>
-      <Route path="/" exact element={<ListSubjects/>}/>
-      <Route path="/:id" exact element={<Subject/>}/>
-      <Route path="/login" exact element={<User/>}/>
+      <Route path="/login" exact element={<LogIn/>}/>
+        <Route path="/singup" exact element={<SingUp/>}/>
+        <Route path="/" element={<div><Nav/><ListSubjects/></div>}/>
+        <Route path="/:id" exact element={<div><Nav/><Subject/></div>}/>
+        
       </Routes>
       </BrowserRouter>
     </div>
+    </AuthProvider>
   );
 }
 
-function Nav() {
+export function Nav() {
+  const SingOut=(e)=>{
+    e.preventDefault()
+    app.auth().signOut()
+    console.log('SINGOUT')
+}
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light" id='navbar'>
       <Link className="navbar-brand px-5 py-3" to="/">ErasmusHelper</Link>
@@ -31,9 +44,10 @@ function Nav() {
       </button>
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="navbar-nav">
-          <a className="nav-item nav-link active" href="#">Home </a>
+        <Link className="nav-item nav-link" to="/login">Login</Link>
           <a className="nav-item nav-link" href="#">LA</a>
           <a className="nav-item nav-link" href="#">Account</a>
+          <button type="submit" className="btn mx-3" id="buttonSave" onClick={SingOut}>Sing Out</button>
         </div>
       </div>
     </nav>
